ab2fb66693d28a5403c62ad04b900f2d9ec17f2a
Diego Bonagurio <diego.bonagurio@gmail.com>
2023-05-18 11:58:09 -0300

    aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,25 @@
+import "dotenv/config";
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
+import jwt from "@fastify/jwt";
 
 const app = fastify();
 
 app.register(cors, {
   origin: true,
 });
+app.register(jwt, {
+  secret: "spacetime",
+});
 app.register(memoriesRoutes);
+app.register(authRoutes);
 
 app
   .listen({
     port: 3333,
+    host: "0.0.0.0",
   })
   .then(() => {
     console.log("🚀 HTTP server is running on port 3333");
